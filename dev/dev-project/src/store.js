96476d56b110b9c115b5f0b00de4c1c09ac47aee
Scott Schwab <scott.schwab@gmail.com>
2019-10-05 17:29:28 -0500

    price changes

--- dev/dev-project/src/store.js
+++ dev/dev-project/src/store.js
@@ -6,32 +6,39 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     pumpkins: [
-      ["@ $1.00 mini is ", "1", "0", "mini", "10"],
-      ["@ $5.00 snowball is ", "5", "0", "snowball", "11"],
-      ["@ $6.00 is", "6", "0", "pumpkin_6", "12"],
-      ["@ $8.00 is", "8", "0", "pumpkin_8", "13"],
-      ["@ $10.00 is", "10", "0", "pumpkin_10", "14"],
-      ["@ $12.00 is", "12", "0", "pumpkin_12", "15"],
-      ["@ $14.00 is", "14", "0", "pumpkin_14", "16"],
-      ["@ $16.00 is", "16", "0", "pumpkin_16", "17"],
-      ["@ $18.00 is", "18", "0", "pumpkin_18", "18"],
-      ["@ $20.00 is", "20", "0", "pumpkin_20", "19"],
-      ["@ $24.00 is", "24", "0", "pumpkin_24", "20"],
-      ["@ $30.00 is", "30", "0", "pumpkin_30", "21"]
+      ["@ $0.75 mini is ", "0.75", "0", "mini", "10"],
+      ["@ $1.00 weebes is ", "1.00", "0", "weebes", "11"],
+      ["@ $2.00 spookies is ", "2", "0", "spookies", "12"],
+      ["@ $2.00 snowflake is ", "2", "0", "snowflake", "13"],
+      ["@ $4.00 snowball is ", "4", "0", "snowball", "14"],
+      ["@ $4.00 is", "4", "0", "pumpkin_4", "15"],
+      ["@ $6.00 is", "6", "0", "pumpkin_6", "16"],
+      ["@ $8.00 is", "8", "0", "pumpkin_8", "17"],
+      ["@ $10.00 is", "10", "0", "pumpkin_10", "18"],
+      ["@ $12.00 is", "12", "0", "pumpkin_12", "19"],
+      ["@ $14.00 is", "14", "0", "pumpkin_14", "20"],
+      ["@ $16.00 is", "16", "0", "pumpkin_16", "21"],
+      ["@ $18.00 is", "18", "0", "pumpkin_18", "22"],
+      ["@ $20.00 is", "20", "0", "pumpkin_20", "23"],
+      ["@ $25.00 is", "25", "0", "pumpkin_25", "24"],
+      ["@ $30.00 is", "30", "0", "pumpkin_30", "25"],
+      ["@ $35.00 is", "35", "0", "pumpkin_35", "26"]
     ],
     gourds: [
-      ["large @ $1.00 is", "1", "0", "gourd_large", "30"],
-      ["small @ $0.75 is ", "0.75", "0", "gourd_small", "31"],
-      ["large winged @ $1.50 is", "1.5", "0", "winged_large", "32"],
-      ["small winged @ $1.00 is", "1", "0", "winged_small", "33"],
-      ["gizmo @ $3.00 is", "3", "0", "gizmo", "34"]
+      ["small elf house @ $1.50 is", "1.5", "0", "small_elf_house", "30"],
+      ["large elf house @ $3.00 is", "3", "0", "large_elf_house", "31"],
+      ["large @ $1.00 is", "1", "0", "gourd_large", "33"],
+      ["small @ $0.75 is ", "0.75", "0", "gourd_small", "34"],
+      ["large winged @ $1.50 is", "1.5", "0", "winged_large", "35"],
+      ["small winged @ $1.00 is", "1", "0", "winged_small", "36"],
+      ["gizmo @ $4.00 is", "4", "0", "gizmo", "37"],
+      ["swan @ $8.00 is", "8", "0", "swan", "38"]
     ],
     other: [
       ["$0.50 pumpkin face sticker is ", "0.5", "0", "sticker", "50"],
       ["$0.25 tealight candle is", "0.25", "0", "candle", "51"],
-      ["$0.50 spider kit is ", "0.5", "0", "spider", "52"],
-      ["small mum @ $5.00 is", "5", "0", "small_mum", "53"],
-      ["large mum @ $12.00 is", "12", "0", "large_mum", "54"]
+      ["small mum @ $7.00 is", "7", "0", "small_mum", "53"],
+      ["large mum @ $14.00 is", "14", "0", "large_mum", "54"]
     ],
     invoice: new Map(),
 
